9c960966175c0ec2e256e6f617fe878a7c3d6c98
Jigar Khalas <jigar.khalas@zuru.com>
2024-04-01 18:18:45 +0530

    Added full width to console

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+import * as process from 'process';
+
 export class Log {
     static success(msg:string){
-        console.log(`\x1b[30m \x1b[42m ${msg} \x1b[0m`);
+        console.log(`\x1b[30m \x1b[42m ${msg.padEnd(process.stdout.columns || 80)} \x1b[0m`);
     }
     static danger(msg:string){
-        console.log(`\x1b[30m \x1b[41m ${msg} \x1b[0m`);
+        console.log(`\x1b[30m \x1b[41m ${msg.padEnd(process.stdout.columns || 80)} \x1b[0m`);
     }
     static info(msg:string){
-        console.log(`\x1b[30m \x1b[43m ${msg} \x1b[0m`)
+        console.log(`\x1b[30m \x1b[43m ${msg.padEnd(process.stdout.columns || 80)} \x1b[0m`)
     }
 }
