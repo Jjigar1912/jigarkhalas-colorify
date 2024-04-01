97e2eee21c41e14c77ef3e7c5a7dd181c8f4107a
Jigar Khalas <jigar.khalas@zuru.com>
2024-04-01 18:05:39 +0530

    Completed color

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export class Log {
     static success(msg:string){
-        console.log(`%c {msg}`,'color : black ; background : green');
+        console.log(`\x1b[30m \x1b[42m ${msg} \x1b[0m`);
     }
     static danger(msg:string){
-        console.log(`%c ${msg}`,'color : black ; background : red')
+        console.log(`\x1b[30m \x1b[41m ${msg} \x1b[0m`);
     }
     static info(msg:string){
-        console.log(`%c ${msg}`,'color : black ; background : yellow')
+        console.log(`\x1b[30m \x1b[43m ${msg} \x1b[0m`)
     }
 }
